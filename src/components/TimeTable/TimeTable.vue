d1577e2b8261e72c36a400c3ded2381a00cf7db0
HongKuiFan <1628734237@qq.com>
2023-12-26 19:35:30 +0800

    新增了今日课程表功能

---
+++ src/components/TimeTable/TimeTable.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { dataSource, columns } from './data.ts'
+
+</script>
+
+<template>
+  <a-card hoverable :bordered="false" >
+    <a-table :dataSource="dataSource" :columns="columns" 
+    bordered size="small" :pagination="false">
+        <template #bodyCell="{ column, record }">
+            <template v-if ="record[column.key]==='231'">
+            <a-tag color='purple'>{{ record[column.key]  }}</a-tag>
+            </template>
+            <template v-if ="record[column.key]==='232'">
+            <a-tag color='cyan'>{{ record[column.key]  }}</a-tag>
+            </template>
+        </template>
+    </a-table>
+    <a-card-meta  title="今日课表" description="2023年12月20日更新版" style="padding-top: 20px;">
+      <template #avatar>
+      </template>
+    </a-card-meta>
+  </a-card>
+</template>
